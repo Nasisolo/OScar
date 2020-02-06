e5ce43f38539c8f392d7046ae338a6aab846e105
Nasi <monan.nasir@gmail.com>
2020-02-06 11:29:48 +0100

    RadioButton for Orientation added in StartActivity.java

--- app/src/main/java/it/unive/dais/legodroid/app/StartActivity.java
+++ app/src/main/java/it/unive/dais/legodroid/app/StartActivity.java
@@ -9,6 +9,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -16,6 +21,59 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        EditText MsizeValue = (EditText)findViewById(R.id.MsizeValue);
+        EditText NsizeValue = (EditText)findViewById(R.id.NsizeValue);
+
+        EditText MinitValue = (EditText)findViewById(R.id.MinitValue);
+        EditText NinitValue = (EditText)findViewById(R.id.NinitValue);
+
+        RadioGroup radioOrientation = (RadioGroup)findViewById(R.id.radioOrientation);
+
+        radioOrientation.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch(checkedId){
+                    case R.id.radioNorth:
+                        Toast.makeText(getApplicationContext(),"NORD", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioEast:
+                        Toast.makeText(getApplicationContext(), "EST", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioSouth:
+                        Toast.makeText(getApplicationContext(), "SUD", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioWest:
+                        Toast.makeText(getApplicationContext(), "OVEST", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+        RadioButton radioNorth = (RadioButton)findViewById(R.id.radioNorth);
+        RadioButton radioEast = (RadioButton)findViewById(R.id.radioEast);
+        RadioButton radioSouth = (RadioButton)findViewById(R.id.radioSouth);
+        RadioButton radioWest = (RadioButton)findViewById(R.id.radioWest);
+
+
+        Button buttonEnter = (Button)findViewById(R.id.buttonEnter);
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioOrientation.getCheckedRadioButtonId();
+
+                if(selectedId == radioNorth.getId())
+                    Toast.makeText(getApplicationContext(), "NOOOOOOOOOOOOOOOOORD", Toast.LENGTH_SHORT).show();
+                if(selectedId == radioEast.getId())
+                    Toast.makeText(getApplicationContext(), "EEEEEEEEEEEEEEEEEEST", Toast.LENGTH_SHORT).show();
+                if(selectedId == radioSouth.getId())
+                    Toast.makeText(getApplicationContext(), "SUUUUUUUUUUUUUUUUUUD", Toast.LENGTH_SHORT).show();
+                if(selectedId == radioWest.getId())
+                    Toast.makeText(getApplicationContext(), "OOOOOOOOOOOOOOOOOVEST", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
 
 
     }
